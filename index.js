afa7d87284fe4ed479cdafc7f7ed126a61a108e9
samuel Emmanuel <Eminiheminez@gmail.com>
2025-12-04 17:46:46 +0100

    added some body

--- index.js
+++ index.js
@@ -13,3 +13,33 @@ function hidemenu(){
     bars.style.display = 'block'
     cancel.style.display = 'none'
 }
+
+let possibilities = ['HTML','CSS','JavaScript','React']
+
+let space = document.getElementById('changingspace')
+
+let i =0 
+let count = 0;
+
+ setInterval(() => {
+
+    space.style.opacity = 0;
+
+
+     if (i < possibilities.length){
+   let gorro = possibilities[count++]
+
+   if(count === 4){
+    count = 0;    
+   }
+       setTimeout(()=>{
+        space.style.opacity =1
+           document.getElementById('changingspace').textContent = gorro
+    },600)
+
+    // console.log(gorro)
+    // console.log(count)
+}
+}, 2000);
+
+
