26bcbb1d757f625076f8c764a3a76b340ee10620
Samuel Emmanuel <Eminiheminez@gmail.com>
2025-12-19 00:29:51 +0100

    made little adjustment

--- rps.js
+++ rps.js
@@ -1,8 +1,42 @@
 let computermove = ''
 let result = ''
 
-function setcomputermove(){
-    let randomnum = Math.random()
+
+    // let randomnum = Math.random()
+    // console.log(randomnum)
+
+    // if(randomnum >= 0 && randomnum <1/3){
+    //     computermove = 'Rock'
+    //     console.log(computermove)
+    // } else if (randomnum >=1/3 && randomnum <2/3){
+    //     computermove = 'Paper'
+    // }else if (randomnum >=2/3 && randomnum <=3/3){
+    //     computermove = 'Scissors'
+    // }
+
+    // console.log(computermove)
+
+
+
+
+// document.getElementById('rock').addEventListener('click',function(){
+//     if(computermove === 'Rock'){
+//         result = 'you tie'
+        
+//     }else if (computermove === 'Paper'){
+//         result = 'You Lose'
+
+//     }else if (computermove === 'Scissors'){
+//         result = 'You Win'
+//     }
+
+//     console.log(`you picked rock ,computer picked ${computermove} you ${result}`)
+//     document.getElementById('pick').textContent = `you picked rock ,computer picked ${computermove} you ${result}`
+// })
+
+function add (){
+
+        let randomnum = Math.random()
     console.log(randomnum)
 
     if(randomnum >= 0 && randomnum <1/3){
@@ -14,13 +48,8 @@ function setcomputermove(){
         computermove = 'Scissors'
     }
 
-    console.log(computermove)
-}
 
-setcomputermove()
-
-document.getElementById('rock').addEventListener('click',function(){
-    if(computermove === 'Rock'){
+       if(computermove === 'Rock'){
         result = 'you tie'
         
     }else if (computermove === 'Paper'){
@@ -32,4 +61,5 @@ document.getElementById('rock').addEventListener('click',function(){
 
     console.log(`you picked rock ,computer picked ${computermove} you ${result}`)
     document.getElementById('pick').textContent = `you picked rock ,computer picked ${computermove} you ${result}`
-})
+
+}
