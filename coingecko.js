4085afa9baa79d5101187c8b2fbf0c883fb8a6d2
samuel Emmanuel <eminiheminez@gmail.com>
2026-03-23 20:12:09 +0100

    on it

--- coingecko.js
+++ coingecko.js
@@ -3,14 +3,11 @@ function toggleSwitch(element){
 }
 
  const apiKey = "CG-M64VHYTiBjBmiaia8xqJtU1q";
-//  let oldvalue = null
-
  function getmarketcap(){
      let globalsignalicons = document.querySelectorAll(".globalredorgreen")
  fetch(`https://api.coingecko.com/api/v3/global?x_cg_demo_api_key=${apiKey}`)
   .then(res => res.json())
   .then(data => {
-
 
 // for coins ///////////////////
     let coins = data.data.active_cryptocurrencies.toLocaleString()
@@ -25,12 +22,6 @@ function toggleSwitch(element){
     ////////////////// for marketcap
     let marketcapnumbers =  Math.round(data.data.total_market_cap.usd)
 
-    //  let newvalue = marketcapnumbers
-
-
-
-    // if(oldvalue !== null){
-
     // let globalpercentage = ((newvalue-oldvalue)/oldvalue) * 100
     let globalpercentage = data.data.market_cap_change_percentage_24h_usd
     // console.log(globalpercentage)
@@ -39,8 +30,6 @@ function toggleSwitch(element){
         alls.innerText =  `${globalpercentage.toFixed(1)}%`
     })
 
-    
-        // if(newvalue >oldvalue){
         if(globalpercentage >= 0){
             // console.log('it is green')
              
@@ -48,7 +37,6 @@ function toggleSwitch(element){
              gsi.classList.remove("bi-caret-down-fill");
               gsi.classList.add("bi-caret-up-fill");
 });
-        // }else if(newvalue < oldvalue){
         }else if(globalpercentage < 0){
             // console.log('it is red')
             
@@ -58,15 +46,6 @@ function toggleSwitch(element){
 });
 
         }else {console.log('nothing changed')}
-    // }
-    // oldvalue = newvalue
-    // console.log('old value is:',oldvalue)
-
-    // console.log('newvalue:',newvalue)
-
-
-
-
 
 
     let marketcap = marketcapnumbers.toLocaleString('en-US')
@@ -105,36 +84,149 @@ getmarketcap()
 
 setInterval(getmarketcap,60000)
 
-//  fetch(`https://api.coingecko.com/api/v3/global?x_cg_demo_api_key=${apiKey}`)
+// Trending box
+
+
+
+fetch('https://api.coingecko.com/api/v3/search/trending')
+  .then(res => res.json())
+  .then(data => {
+    // console.log(data);
+
+    let coins = data.coins.slice(0,3);
+    // console.log(coins)
+
+
+    const promises = coins.map(coining=>{
+        coin = coining.item
+        // console.log(coin)   
+        let coinid = coin.id
+        // console.log(coin.id)
+
+
+
+
+
+
+    return fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=${coinid}`)
+       .then(res => res.json())
+    //    .then(data=>{console.log(data)})
+
+    })
+
+    return Promise.all(promises)
+
+    .then(allData=>{
+        allData.forEach(coinArray=>{
+            console.log(coinArray)
+            const coinData = coinArray[0];
+            console.log(coinData)
+                  document.getElementById('namesandicons').innerHTML += `
+        <li class="flexingnameandicon">
+          <img src="${coinData.image}" />
+          <span class="coinname">${coinData.name}</span>
+        </li>
+      `;
+
+
+      let pricepercent = coinData.price_change_percentage_24h
+
+    //   if(pricepercent >= 0){
+    //     document.getElementById("arrow").innerHTML = `<i class="bi bi-caret-up-fill"></i>`
+    //   }else if(pricepercent < 0){
+    //     document.getElementById("arrow").innerHTML = `<i class="bi bi-caret-down-fill"></i>`
+    //   }
+
+
+            document.getElementById('priceandchanges').innerHTML += `
+        <li class="flexingnameandicon">
+          <span class="coinpricechange">$${coinData.current_price}${pricepercent > 0 ? '<i class="bi bi-caret-up-fill"></i>' : '<i class="bi bi-caret-down-fill"></i>'}${coinData.price_change_percentage_24h}%</span>
+        </li>
+      `;
+        })
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  })
+
+        
+
+
+
+
+
+
+    //     document.getElementById('namesandicons').innerHTML += `
+    //     <li class="flexingnameandicon">
+    //       <img  src="${coin.small}" />
+    //        <span class="coinname">${coin.name}</span>
+    //        </li>
+    //    `
+
+    //            document.getElementById('priceandchanges').innerHTML += `
+    //     <li class="flexingnameandicon">
+    //        <span class="coinprice">$${(coin.data.price).toFixed(2)}</span> 
+        
+    //        </li>
+    //    `
+
+
+
+    
+    // })
+// });
+
+
+
+
+// fetch('https://api.coingecko.com/api/v3/search/trending')
 //   .then(res => res.json())
 //   .then(data => {
-//     // console.log(data)
-//     ////////////////// for marketcap
-//     let marketcapnumbers =  Math.round(data.data.total_market_cap.usd)
-//     let marketcap = marketcapnumbers.toLocaleString('en-US')
-//     let short = (marketcapnumbers/1e12).toFixed(3)
-//     document.getElementById('marketshort').innerText = '$'+short+'T'
-//     console.log(Math.round(marketcapnumbers))
-//     document.getElementById('totalmarketcap').innerText = '$'+marketcap
+//     let coins = data.coins.slice(0, 3); // top 3 trending coins
 
-//     // ///////////////for 24hr trading volumne
-//     let tradingvol = Math.round(data.data.total_volume.usd)
-//     let volumeshort = (tradingvol/1e9).toFixed(3)
-//     document.getElementById('volumeshort').innerText = `$${volumeshort}T`
-//     let hrschange = tradingvol.toLocaleString('en-US')
-//     document.getElementById('24hrsvol').innerText = '$'+hrschange
+//     // fetch market data for each coin
+//     const promises = coins.map(coinWrapper => {
+//       const coin = coinWrapper.item; // extract the actual coin
+//       return fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=${coin.id}`)
+//         .then(res => res.json());
+//     });
 
-//     /////////////for  BTC Dominance
-//     console.log(data.data.market_cap_percentage.btc); // BTC dominance
-//     let btcdominance = data.data.market_cap_percentage.btc.toFixed(1)
-//     console.log(btcdominance)
-//     let ethdominance = data.data.market_cap_percentage.eth.toFixed(1)
-//     console.log(ethdominance)
-//     document.getElementById('btcdominance').innerText = `BTC ${btcdominance}%  ETH ${ethdominance}%`
+//     // wait for all fetches
+//     Promise.all(promises)
+//       .then(allData => {
+//         // allData[i] corresponds to coins[i]
+//         allData.forEach((coinMarketData, index) => {
+//           const coin = coins[index].item; // original coin info
+//           const market = coinMarketData[0]; // market data from API
 
-//     // //////////for Eth dominanace
-//     console.log(data.data)
+//           document.getElementById('namesandicons').innerHTML += `
+//             <li class="flexingnameandicon">
+//               <img src="${coin.small}" />
+//               <span class="coinname">${coin.name}</span>
+//             </li>
+//           `;
 
+//           document.getElementById('priceandchanges').innerHTML += `
+//             <li class="flexingnameandicon">
+//               <span class="coinprice">$${market.current_price.toFixed(2)}</span>
+//             </li>
+//           `;
+//         });
+//       })
+//       .catch(err => console.error(err));
 //   })
-
 //   .catch(err => console.error(err));
--- erepa.js
+++ erepa.js
@@ -6,68 +6,103 @@
 // let short = (numstr/1e9).toFixed(3)
 // // console.log(numstr.slice(0,3))
 
-// console.log(short)
-
-
  const apiKey = "CG-M64VHYTiBjBmiaia8xqJtU1q";
-
-//  fetch(`https://api.coingecko.com/api/v3/global?x_cg_demo_api_key=${apiKey}`)
-//   .then(res => res.json())
-//   .then(data => {
-//     // console.log(data)
-//     ////////////////// for marketcap
-//     let marketcapnumbers =  Math.round(data.data.total_market_cap.usd)
-//     let marketcap = marketcapnumbers.toLocaleString('en-US')
-//     let short = (marketcapnumbers/1e12).toFixed(3)
-//     // console.log(marketcapnumbers)
-//     // document.getElementById('marketshort').innerText = '$'+short+'T'
-//     // console.log(Math.round(marketcapnumbers))
-//     // document.getElementById('totalmarketcap').innerText = '$'+marketcap
-
-
-//     let oldvalue = null;
-//     let newvalue = marketcapnumbers
-
-//     if(oldvalue !== null){
-//         if(newvalue >oldvalue){
-//             console.log('it is green')
-//         }else if(newvalue < oldvalue){
-//             console.log('it is red')
-//         }else {console.log('nothing changed')}
-//     }
-//     oldvalue = newvalue
-//   })
-//   .catch(err=>console.error(err))
-
-    let oldvalue = null;
-
-  setInterval(() => {
-    //  fetch(`https://api.coingecko.com/api/v3/global?x_cg_demo_api_key=${apiKey}`)
+fetch('https://api.coingecko.com/api/v3/search/trending')
   .then(res => res.json())
   .then(data => {
-    // console.log(data)
-    ////////////////// for marketcap
-    let marketcapnumbers =  Math.round(data.data.total_market_cap.usd)
-    let marketcap = marketcapnumbers.toLocaleString('en-US')
-    let short = (marketcapnumbers/1e12).toFixed(3)
-    // console.log(marketcapnumbers)
-    // document.getElementById('marketshort').innerText = '$'+short+'T'
-    // console.log(Math.round(marketcapnumbers))
-    // document.getElementById('totalmarketcap').innerText = '$'+marketcap
+    // console.log(data);
+
+    let coins = data.coins.slice(0,3);
+    // console.log(coins)
+
+
+    coins.forEach(coin=>{
+        coin = coin.item
+        console.log(coin)
+
+
+        // document.getElementById('testing').innerHTML += `<li class="flexing">${coin.name} <img src="${coin.small}" /> <span>$${(coin.data.price).toFixed(2)}</span></li>`
+        document.getElementById('testing').innerHTML += `
+        
+        
+        <li class="flexing">${coin.name} <img src="${coin.small}" /> <span>$${(coin.data.price).toFixed(2)}</span></li>`
+    })
 
 
 
-    let newvalue = marketcapnumbers
+    // coins.forEach(coin => {
+    //   console.log(coin.item.name, coin.item.price_btc);
+    // });
+  });
 
-    if(oldvalue !== null){
-        if(newvalue >oldvalue){
-            console.log('it is green')
-        }else if(newvalue < oldvalue){
-            console.log('it is red')
-        }else {console.log('nothing changed')}
-    }
-    oldvalue = newvalue
-    console.log('old value is:',oldvalue)
-  })
-  .catch(err=>console.error(err))
-  }, 2000);
+
+
+
+  // fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin')
+  // .then(res => res.json())
+  // .then(marketData => {
+  //   console.log(marketData);
+  // });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//   Promise.all([
+//   fetch('https://api.coingecko.com/api/v3/search/trending').then(res => res.json()),
+//   fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=bitcoin').then(res => res.json())
+// ])
+// .then(([trendingData, marketData]) => {
+
+//   let coins = trendingData.coins.slice(0,3);
+
+//   coins.forEach(coin => {
+//     coin = coin.item;
+
+//     // find matching market data
+//     let market = marketData.find(m => m.id === coin.id);
+
+//     let price = market ? market.current_price : 0;
+
+
+
+//     document.getElementById('testing').innerHTML += `
+//       <li class="flexingnameandicon">
+//         <span class="coinprice">$${price}</span>
+//       </li>
+//     `;
+//   });
+
+// });
+
+
+  
